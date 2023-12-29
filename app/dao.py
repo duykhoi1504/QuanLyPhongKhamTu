b4b5565ff87a4bf7c562545a035d873ab25def2d
duykhoi1504 <voduykhoi1504@gmail.com>
2023-12-29 14:41:53 +0700

    push

---
+++ app/dao.py
@@ -0,0 +1,43 @@
+def load_categories():
+    return[{
+        'id':1,
+        'name':'Mobile'
+
+    },{
+        'id':2,
+        'name':'Tablet'
+    }]
+
+
+def load_products(kw=None):
+    return [{
+        'id':1,
+        'name':'Mobile',
+        'price':20000000,
+        'image':'https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-pro-max_3.png'
+    },{
+        'id':2,
+        'name':'tablet',
+        'price':40000000,
+        'image':'https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-27.png'
+    },{
+        'id':3,
+        'name':'pc',
+        'price':90000000,
+        'image':'//product.hstatic.net/200000722513/product/5000d_white_aero_61797e20d29a47ff9f7589071a5099da_grande.png'
+    },{
+        'id':4,
+        'name':'Mobile',
+        'price':20000000,
+        'image':'https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:80/plain/https://cellphones.com.vn/media/catalog/product/i/p/iphone-15-pro-max_3.png'
+    },{
+        'id':5,
+        'name':'tablet',
+        'price':40000000,
+        'image':'https://cdn.hoanghamobile.com/i/preview/Uploads/2021/09/15/image-removebg-preview-27.png'
+    },{
+        'id':6,
+        'name':'pc',
+        'price':90000000,
+        'image':'//product.hstatic.net/200000722513/product/5000d_white_aero_61797e20d29a47ff9f7589071a5099da_grande.png'
+    }]
